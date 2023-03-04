9c65304ac91277b6d66f8da78ca94f021dce8151
ryosuke <ryou1901@gmail.com>
2023-03-04 18:25:25 +0900

    add sample_app

---
+++ Sources/sample‗app/sample_app.swift
@@ -0,0 +1,6 @@
+public struct sample_app {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/sample‗appTests/sample_appTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import sample_app
+
+final class sample_appTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(sample_app().text, "Hello, World!")
+    }
+}
